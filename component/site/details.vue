d9b373a61e91e0f2de9f6ccd866f68462956f148
Limont <1261750677@qq.com>
2023-09-08 15:46:42 +0800

    chore(): 子模块配置

--- component/site/details.vue
+++ component/site/details.vue
@@ -49,8 +49,7 @@ export default {
       loading: '',
       active: false,
       params: {
-        id: '',
-        // weather: 2,
+        id: ''
       },
       details: {},
       activeName: 'overview'
